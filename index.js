40992f71a03ef65cf933163eb23995af565bdf2d
Brian Ladwig <br.ladwig@gmail.com>
2022-01-26 11:58:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+ 
+ 
+ 
+ 
+ const btn = document.querySelector('#btn');
+ 
+ 
+
+
+ btn.addEventListener('click', async () => {
+
+     const username = document.querySelector('#username-search').value
+     const gitRepos = `https://api.github.com/users/${username}/repos`;
+     const response = await fetch(gitRepos);
+     const repos = await response.json();
+
+    
+
+
+
+     repos.forEach(item => {
+
+        console.log(Object.keys(item));
+
+         const ul = document.querySelector('#repoBox');
+        
+         const link = document.createElement('a');
+         link.href = item.html_url;
+         link.classList.add('list-group-item-action')
+         
+         ul.appendChild(link);
+         link.classList.add('list-group-item');
+        
+
+
+         const repoName = document.createElement('h3');
+         link.appendChild(repoName);
+         repoName.innerText = item.name;
+
+
+         const description = document.createElement('p');
+         link.appendChild(description);
+         description.innerText = item.description;
+
+     });
+
+ });
+
+
+
+
+
+
+
+
+
+ 
+
